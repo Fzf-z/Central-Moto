d6edb9a3b9085cd1909702f46eddba7d48c6a7a8
FZF <fabriciozeballos@gmail.com>
2020-04-15 21:15:56 -0300

    1

---
+++ lib/ButtonKing/README.md
@@ -0,0 +1,33 @@
+![Logo](https://user-images.githubusercontent.com/47109201/60978866-fa684680-a35b-11e9-9203-cd22866844c6.png)
+
+[![Build Status](https://travis-ci.org/TanPitch/ButtonKing.svg?branch=master)](https://travis-ci.org/TanPitch/ButtonKing)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/1cd1f2b79232424a98b6c1aa227dee7d)](https://app.codacy.com/app/TanPitch/ButtonKing?utm_source=github.com&utm_medium=referral&utm_content=TanPitch/ButtonKing&utm_campaign=Badge_Grade_Dashboard)
+[![Source Line of Code](https://img.shields.io/badge/Lines%20Of%20Code-380-brightgreen.svg)](https://github.com/TanPitch/ButtonKing)
+
+This library is improving the usage of a simple singe button.
+It takes a single pushbutton to do some of the typical button press events like single clicks, double clicks and long-time pressing.
+This will make your project less buttons and much more funtion inside it.
+
+### lasted version release
+
+ButtonKing 1.0.2 ([download here](https://github.com/TanPitch/ButtonKing/releases/tag/1.0.2))
+
+* some minor improvement
+* example file bugs fixed
+* all worked : test on
+  * Arduino Zero, Leonardo, Uno, Mega, Due
+  * ESP8266 and ESP32
+  * STM32 based boards
+
+to install ButtonKing library
+[install instruction](https://github.com/TanPitch/ButtonKing/wiki/How-to-install-ButtonKing)
+
+### check lists
+
+- [x] short/long period for button pressing.
+- [x] double click with button pressing.
+- [ ] tripple click
+- [ ] click for the specific times before run-out.
+
+for more information
+[Button King wiki](https://github.com/TanPitch/ButtonKing/wiki)
---
+++ src/main.cpp
@@ -0,0 +1,190 @@
+//==========================================================================================
+//----------------------------------- MOTO -------------------------------------------------
+//==========================================================================================
+#include <Arduino.h>
+#include <ButtonKing.h>
+
+//-------------------------------- Pulsadores-----------------------------------------------
+ButtonKing pulsadorArranque(A0, true);	//A0
+ButtonKing pulsadorDerecho(A1,true);	//A1
+ButtonKing pulsadorIzquierdo(A2,true);	//A2
+ButtonKing pulsadorLUZ(A3,true);		//A3
+
+const byte pulsFreno = A4;				// Pulsadores frenos
+const byte pulsAux = A5;				// Pulasor Auxiliar
+
+//--------------------------------- Luces --------------------------------------------------
+const byte led_freno = 11;				// luz de freno
+const byte led_senI = 10;				// señalero izquierdo
+const byte led_senD = 9;				// señalero derecho 
+const byte Luz = 6;						// relé de luz corta
+const byte LuzALTA = 5;					// relé de luz alta
+
+//--------------------------------------- Otros --------------------------------------------
+const byte arranque = 3;				// Arranque
+//const byte bocina = 16;				// Bocina
+
+//------------------------------------- Variables ------------------------------------------
+
+
+//funcion Destellador
+boolean senalDestellador = LOW;
+long tP1 = 0;
+long tP2 = 0;
+byte sena = 0;
+
+//Funcion Señaleros
+byte tiempoParpadeo = 200;
+
+//Funcion Luces
+boolean estadosLuces = LOW;
+byte intencidadLedFreno = 70;
+boolean estadoLuzAlta = LOW;
+
+//funcion Freno
+boolean freno_act = false;
+
+//==========================================================================================
+//---------------------------------------- Funciones ---------------------------------------
+//==========================================================================================
+boolean Destellador(byte TP){
+	tP2 = millis();
+	if(tP2 - tP1 > TP){
+		tP1 = tP2;
+		(senalDestellador == LOW) ? senalDestellador = HIGH : senalDestellador = LOW;
+	}  
+	return senalDestellador;
+}
+
+void Senaleros(){
+	if(sena == 1){
+	    digitalWrite(led_senD, Destellador(tiempoParpadeo));
+	    digitalWrite(led_senI, LOW); 
+	}
+	if(sena == 2){
+	    digitalWrite(led_senI, Destellador(tiempoParpadeo));
+	    digitalWrite(led_senD, LOW); 
+	}
+	if(sena == 3){
+	    digitalWrite(led_senD, Destellador(tiempoParpadeo));
+	    digitalWrite(led_senI, Destellador(tiempoParpadeo));
+	}
+	else
+	{
+		digitalWrite(led_senD, LOW);
+		digitalWrite(led_senI, LOW);
+	}
+}
+
+void Luces(){
+	if(estadosLuces == HIGH){
+		digitalWrite(Luz, HIGH);
+		analogWrite(led_freno, intencidadLedFreno);
+	}
+	else{
+		digitalWrite(Luz, LOW);
+		analogWrite(led_freno, LOW);
+		digitalWrite(LuzALTA, LOW);
+	}
+	(estadoLuzAlta) ? digitalWrite(LuzALTA, HIGH) : digitalWrite(LuzALTA, LOW);
+}
+
+void Freno(){
+	if(freno_act){
+		for(int i=0; i<255; i++){
+			analogWrite(led_freno, i);
+		}
+	}
+}
+
+// ----------------------------- Boton Arranque -------------------------------------
+void ArranqueStop(){
+	digitalWrite(arranque, LOW);
+}
+void ArranqueStart(){
+	digitalWrite(arranque, HIGH);
+}
+					
+//-------------------------------- Boton Luces --------------------------------------
+void EnciendeLuces(){
+	estadosLuces == LOW ? estadosLuces = HIGH : estadosLuces = LOW;
+}
+void EncenderLuzAlta(){
+	estadoLuzAlta == LOW ? estadoLuzAlta = HIGH : estadoLuzAlta = LOW;
+}
+void DestellarLuzAlta(){
+	estadoLuzAlta = LOW;
+	digitalWrite(LuzALTA, Destellador(50));
+}
+
+//------------------------------ Boton Señalero -------------------------------------
+void SenaleroDerechoEncendido(){
+	sena==1 ? sena=0 : sena=1;			//Operador Ternario
+}
+void SenaleroIzquieroEncendido(){		//Operador Ternario
+	sena==2 ? sena=0 : sena=2;
+}
+void SenaleroApagado(){
+	sena = 0;
+}
+void Baliza(){
+	sena = 3;
+}
+
+//==========================================================================================
+//------------------------------------- Setup ----------------------------------------------
+//==========================================================================================
+
+void setup(){
+
+//------------------------------------- Salidas -----------------------
+	pinMode(LuzALTA, OUTPUT);
+	pinMode(led_freno, OUTPUT);
+	pinMode(led_senI, OUTPUT);
+	pinMode(led_senD, OUTPUT);
+	pinMode(Luz, OUTPUT);
+	pinMode(arranque, OUTPUT);
+
+//------------------------------------- Entradas ---------------------
+	pinMode(pulsFreno, INPUT);
+	
+	//------------------- Boton Arranque
+	pulsadorArranque.setShortDoubleStart(ArranqueStart);
+	pulsadorArranque.setLongDoubleStop(ArranqueStop);
+
+	//------------------- Boton Luces
+	pulsadorLUZ.setDoubleClick(EnciendeLuces);
+	pulsadorLUZ.setClick(EncenderLuzAlta);
+	pulsadorLUZ.setLongClickStart(DestellarLuzAlta);
+
+	//------------------- Boton Derecho
+	pulsadorDerecho.setClick(SenaleroDerechoEncendido);
+	pulsadorDerecho.setDoubleClick(SenaleroApagado);
+	pulsadorDerecho.setLongClickStart(Baliza);
+
+	//------------------- Boton Izquierdo
+	pulsadorIzquierdo.setClick(SenaleroIzquieroEncendido);
+	pulsadorIzquierdo.setDoubleClick(SenaleroApagado);	
+	pulsadorIzquierdo.setLongClickStart(Baliza);
+
+	//------------------- Boton Freno
+
+
+}
+
+//==========================================================================================
+//-------------------------------------- Loop ----------------------------------------------
+//==========================================================================================
+
+void loop(){
+	(!digitalRead(pulsFreno)) ? freno_act = true : freno_act = false;
+	pulsadorIzquierdo.isClick();
+	pulsadorDerecho.isClick();
+	pulsadorLUZ.isClick();
+	pulsadorArranque.isClick();
+	Luces();
+	Freno();
+	Senaleros();
+
+}
+
